941ce7f9397d37ab5fbcfa9d774f9b749bec0c32
Justin Mayerick <jmayeric@gmail.com>
2016-10-06 22:54:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 function getFirstSelector(selector) {
-  
+  return document.querySelector(selector)
 }
 
 function nestedTarget() {
-
+  return document.querySelector("#nested .target")
 }
 
 function deepestChild() {
-
+  return document.querySelector("#grand-node")
 }
 
-function increaseRankBy() {
-
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('.ranked-list')
+  for (let i = 0, l = list.length; i < l; i++) {
+    list[i].innerHTML = (i + n).toString()
+  }
 }
